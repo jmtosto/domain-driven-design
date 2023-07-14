0d5840f707c6260eee5c62e8c10e0d99d534f9fa
João Tosto <48574634+jmtosto@users.noreply.github.com>
2023-07-14 15:30:34 -0300

    refactor: fixes imports on main file

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Customer from './entity/customer';
-import Address from './entity/address';
-import OrderItem from './entity/order_item';
-import Order from './entity/order';
+import Customer from './domain/entity/customer';
+import Address from './domain/entity/address';
+import OrderItem from './domain/entity/order_item';
+import Order from './domain/entity/order';
 
 let customer = new Customer("customer-1", "João Tosto");
 const address = new Address("Lombard Street", 1000, "94109", "San Francisco");
